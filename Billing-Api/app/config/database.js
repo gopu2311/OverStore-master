1477281ca49f1e3e453e4503197a0b26ca3ce793
gopu2311 <157279850+gopu2311@users.noreply.github.com>
2025-05-23 12:38:31 +0530

    Update database.js

--- Billing-Api/app/config/database.js
+++ Billing-Api/app/config/database.js
@@ -1,17 +1,21 @@
 const mysql = require('mysql2');
 
 const connectWithRetry = () => {
-  const connection = mysql.createConnection({
+  const config = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'billing',
-  });
+  };
+
+  console.log("Trying to connect with config:", config);
+
+  const connection = mysql.createConnection(config);
 
   connection.connect((err) => {
     if (err) {
-      console.error('DB connection failed, retrying in 5s...', err);
-      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+      console.error('DB connection failed, retrying in 5s...', err.message);
+      setTimeout(connectWithRetry, 5000);
     } else {
       console.log('Connected to MySQL');
     }
